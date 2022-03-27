0762154453dd0850030f6db86313995e70a9300a
Titus Nguyen <titusstar007@gmail.com>
2022-03-26 19:00:59 -0700

    Added working About.jsx, file paths not fixed

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter, Routes, Route}  from "react-router-dom";
 import './index.css';
 import App from './App.jsx';
+import About from './About.jsx'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
-        <Route path="about" element={<App/>} />
+        <Route path="about" element={<About/>} />
         <Route path="portfolio" element={<App/>} />
         <Route path="pokedex" element={<App/>} />
       </Route>
