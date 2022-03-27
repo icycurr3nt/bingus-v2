82252bc4be070843787c327fe9b353f4bc644cbb
Titus Nguyen <titusstar007@gmail.com>
2022-03-26 18:28:45 -0700

    First Template Refractor for React Router

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+    <footer class="footer mt-lg-2 py-3 bg-dark">
+      <div class="container">
+          <span class="text-muted">Creative Commons or something.</span>
+      </div>
+    </footer>
+    )
+}
