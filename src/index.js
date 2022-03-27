0bffd27d3d629e70e8cd4d823185ff016a75dff4
Titus Nguyen <titusstar007@gmail.com>
2022-03-26 19:15:01 -0700

    Changed Title of webpage + deleted un-needed files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Routes, Route}  from "react-router-dom";
+import { BrowserRouter, Routes, Route }  from "react-router-dom";
 import './index.css';
 import App from './App.jsx';
 import About from './About.jsx'
