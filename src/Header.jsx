7ab75685db24b11db30aa38e77b3abe9a2635bd1
Titus Nguyen <titusstar007@gmail.com>
2022-03-28 20:52:00 -0700

    Moved Clock.jsx to Header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Clock from './Clock.jsx'
+
+
+class Clock extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
+    }
+  
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tick(),
+        1000
+      );
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+  
+    tick() {
+      this.setState({
+        date: new Date()
+      });
+    }
+  
+    render() {
+      return (
+        <div>
+          {this.state.date.toLocaleTimeString()}
+        </div>
+      );
+    }
+  }
+
 
 export default function Header() {
     return (
