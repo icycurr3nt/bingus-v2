119d13ada12cca219d9f901e9093d8d8d9f424a6
icycurr3nt <dmvanalstine@gmail.com>
2022-03-29 08:30:07 -0700

    Added working Pokemon fetch request to output

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Pokedex } from "pokeapi-js-wrapper"
 
 
-const P = new Pokedex();
-
-
-
-P.getPokemonByName("1")
-  .then(function(response) {
-  })
-
 export default function Pokemon() {
+  const pokemonId = useParams().pokemonId
+  const [pokemon, setPokemon] = useState();  
+
+  useEffect(() => {
+    const P = new Pokedex();
+    P.getPokemonByName(pokemonId).then((resp) => {setPokemon(resp)})
+
+  }, [pokemonId])
+
   return (
-    <p>{useParams().pokemonId}</p>
+    <div>
+    <img src={pokemon ? pokemon.sprites.front_default : "monke"} alt="Pokemon"/>
+    <p>{JSON.stringify(pokemon)}</p>
+    </div>
   )
 }
