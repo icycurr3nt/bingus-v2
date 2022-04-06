ef0720e957b7ec8a04a0be63780750ed0e163a49
icycurr3nt <dmvanalstine@gmail.com>
2022-04-06 07:41:11 -0700

    started the grocery list

--- src/pages/GroceryList.jsx
+++ src/pages/GroceryList.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { Fragment } from "react";
+import "../assets/css/grocerylist.css";
+
+
+function GroceryItem(props) {
+    <li class="groceryItem">
+         <h2>{props.name}</h2>
+         <p>price: {props.price}</p>
+         <img src={props.image} alt={props.name}></img>
+    </li>
+}
+
+
+function GroceryItems(props) {
+    return( <h1>test</h1>)
+}
+
+function SearchBar() {
+    return (<div id="searchWrapper">
+    <input
+        type="text"
+        name="searchBar"
+        id="searchBar"
+        placeholder="search for an item"
+    />
+</div>)
+}
+
 
 export default function GroceryList() {
-    return (
-        <h1>monke</h1>
+    return(
+        <div class="container">
+        <h1>&#x2728;Grocery List &#x2728;</h1>
+        <SearchBar/>
+        <GroceryItems props={groceryJSON}/>
+        </div>
     )
+    
 }
+
+
