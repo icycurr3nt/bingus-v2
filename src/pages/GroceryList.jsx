f9aee9b7b06e0f2da4e41564617edfa74ca2c4e1
icycurr3nt <dmvanalstine@gmail.com>
2022-04-06 08:26:02 -0700

    Added working GroceryItemList

--- src/pages/GroceryList.jsx
+++ src/pages/GroceryList.jsx
@@ -1,22 +1,36 @@
-import React, { Fragment } from "react";
+import React from "react";
+import groceryItemsJSON from "../assets/files/GroceryItems.json";
 import "../assets/css/grocerylist.css";
 
 
 function GroceryItem(props) {
+    return (
     <li class="groceryItem">
          <h2>{props.name}</h2>
-         <p>price: {props.price}</p>
+         <p>Price: ${props.price} Category: {props.category}</p>
          <img src={props.image} alt={props.name}></img>
     </li>
+    )
 }
 
 
 function GroceryItems(props) {
-    return( <h1>test</h1>)
+    const rows = [];
+        
+    props.groceryItems.forEach((grocery) => {
+        rows.push(
+            <GroceryItem name={grocery.name} price={grocery.price} image={grocery.image}
+                category={grocery.category} key={grocery.name}
+            />
+        );
+    });
+
+    return <ul id="groceryList">{rows}</ul>
 }
 
 function SearchBar() {
-    return (<div id="searchWrapper">
+    return (
+    <div id="searchWrapper">
     <input
         type="text"
         name="searchBar"
@@ -27,15 +41,14 @@ function SearchBar() {
 }
 
 
-export default function GroceryList() {
-    return(
-        <div class="container">
-        <h1>&#x2728;Grocery List &#x2728;</h1>
-        <SearchBar/>
-        <GroceryItems props={groceryJSON}/>
-        </div>
-    )
-    
-}
+export default function GroceryList () {
 
+    return (
+            <div class="container">
+                <h1>&#x2728;Grocery List &#x2728;</h1>
+                <SearchBar/>
+                <GroceryItems groceryItems={groceryItemsJSON}/>
+            </div>
+        )  
+}
 
